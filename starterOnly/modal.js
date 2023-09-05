85bc66370917be975e846c00ea9d76041d9226ab
radouane <radouane.eladlani@gmail.com>
2023-09-05 11:14:56 +0200

    etape 1 fermature de la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,29 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+// On récupère la classe close avec querySelector//
+const closeModal = document.querySelector(".close");
+
+// Si closeModal et egale a null en throw une erreur//
+if (closeModal == null) throw new Error("Élément closeModal pas trouvé");
+
+function closeModalBtn() {
+  if (modalbg == null) throw new Error("Élément modalbg pas trouvé");
+  modalbg.classList.remove("active");
+
+}
+
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModal.addEventListener("click", closeModalBtn);
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  if (modalbg == null) throw new Error("background pas trouvé");
+  modalbg.classList.add("active");
 }
 
 
